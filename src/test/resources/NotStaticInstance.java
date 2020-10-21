c15264ae76a118be70451248c77171464690e5f1
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-10-21 10:31:44 -0300

    Improving not static instance pattern

--- src/test/resources/NotStaticInstance.java
+++ src/test/resources/NotStaticInstance.java
@@ -1,6 +1,7 @@
 public class SomeSingleton {
 
     @AlertIfNotStatic("Instance attribute should be static") 
-    private SomeSingleton instance;
+    @AnyModifier
+    SomeSingleton instance;
     
 }
